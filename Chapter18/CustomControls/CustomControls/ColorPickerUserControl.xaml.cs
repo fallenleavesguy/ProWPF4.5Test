89f836590241d2255f4effba7b0fafd75741aa62
donghs <no@email.com>
2025-09-26 15:55:42 +0800

    update

---
+++ Chapter18/CustomControls/CustomControls/ColorPickerUserControl.xaml.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace CustomControls
+{
+    /// <summary>
+    /// Interaction logic for ColorPickerUserControl.xaml
+    /// </summary>
+    public partial class ColorPickerUserControl : UserControl
+    {
+        public static DependencyProperty ColorProperty;
+        public static DependencyProperty RedProperty;
+        public static DependencyProperty GreenProperty;
+        public static DependencyProperty BlueProperty;
+
+        public Color Color
+        {
+            get { return (Color)GetValue(ColorProperty); }
+            set { SetValue(ColorProperty, value); }
+        }
+        public byte Red
+        {
+            get { return (byte)GetValue(RedProperty); }
+            set { SetValue(RedProperty, value); }
+        }
+        public byte Green
+        {
+            get { return (byte)GetValue(GreenProperty); }
+            set { SetValue(GreenProperty, value); }
+        }
+        public byte Blue
+        {
+            get { return (byte)GetValue(BlueProperty); }
+            set { SetValue(BlueProperty, value); }
+        }
+
+        public ColorPickerUserControl()
+        {
+            ColorProperty = DependencyProperty.Register(
+                "Color",
+                typeof(Color),
+                typeof(ColorPickerUserControl),
+                new FrameworkPropertyMetadata(
+                    Colors.Black,
+                    new PropertyChangedCallback(OnColorChanged)
+                )
+            );
+            RedProperty = DependencyProperty.Register(
+                "Red",
+                typeof(byte),
+                typeof(ColorPickerUserControl),
+                new FrameworkPropertyMetadata(new PropertyChangedCallback(OnColorRGBChanged))
+            );
+            GreenProperty = DependencyProperty.Register(
+                "Green",
+                typeof(byte),
+                typeof(ColorPickerUserControl),
+                new FrameworkPropertyMetadata(new PropertyChangedCallback(OnColorRGBChanged))
+            );
+
+            BlueProperty = DependencyProperty.Register(
+                "Blue",
+                typeof(byte),
+                typeof(ColorPickerUserControl),
+                new FrameworkPropertyMetadata(new PropertyChangedCallback(OnColorRGBChanged))
+            );
+            InitializeComponent();
+        }
+
+        private void OnColorRGBChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+        {
+            ColorPickerUserControl colorPicker = (ColorPickerUserControl)sender;
+            Color color = colorPicker.Color;
+
+            if (e.Property == RedProperty)
+                color.R = (byte)e.NewValue;
+            else if (e.Property == GreenProperty)
+                color.G = (byte)e.NewValue;
+            else if (e.Property == BlueProperty)
+                color.B = (byte)e.NewValue;
+
+            colorPicker.Color = color;
+        }
+
+        private void OnColorChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+        {
+            Color newColor = (Color)e.NewValue;
+
+            ColorPickerUserControl colorPicker = (ColorPickerUserControl)sender;
+            colorPicker.Red = newColor.R;
+            colorPicker.Green = newColor.G;
+            colorPicker.Blue = newColor.B;
+        }
+    }
+}
